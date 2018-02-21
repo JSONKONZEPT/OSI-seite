4842ae09cfe2ab6bd6d622ad213c7dcbfe10a554
Jason Conzett <conzettjason@gmail.com>
2018-02-21 14:01:43 +0100

    changed view and controll of dropdown and added shadow

--- js/osi-layer.js
+++ js/osi-layer.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $(".layer-7").mouseenter(function () {
+   /* $(".layer-7").click(function () {
 
         $(".layer-6").hide();
         $(".layer-5").hide();
@@ -10,17 +10,17 @@ $(document).ready(function () {
         $(".layer-1").hide();
 
 
-    }).mouseleave(function () {
-        $(".layer-6").fadeIn();
-        $(".layer-5").fadeIn();
-        $(".layer-4").fadeIn();
-        $(".layer-3").fadeIn();
-        $(".layer-2").fadeIn();
-        $(".layer-1").fadeIn();
+    }).click(function () {
+        $(".layer-6").fadeIn(100);
+        $(".layer-5").fadeIn(100);
+        $(".layer-4").fadeIn(100);
+        $(".layer-3").fadeIn(100);
+        $(".layer-2").fadeIn(100);
+        $(".layer-1").fadeIn(100);
     });
 
 
-    $(".layer-6").mouseenter(function () {
+    $(".layer-6").mouseover(function () {
 
         $(".layer-5").hide();
         $(".layer-4").hide();
@@ -31,14 +31,14 @@ $(document).ready(function () {
 
     }).mouseleave(function () {
 
-        $(".layer-5").fadeIn();
-        $(".layer-4").fadeIn();
-        $(".layer-3").fadeIn();
-        $(".layer-2").fadeIn();
-        $(".layer-1").fadeIn();
+        $(".layer-5").fadeIn(100);
+        $(".layer-4").fadeIn(100);
+        $(".layer-3").fadeIn(100);
+        $(".layer-2").fadeIn(100);
+        $(".layer-1").fadeIn(100);
     });
 
-    $(".layer-5").mouseenter(function () {
+    $(".layer-5").mouseover(function () {
 
         $(".layer-4").hide();
         $(".layer-3").hide();
@@ -48,13 +48,13 @@ $(document).ready(function () {
 
     }).mouseleave(function () {
 
-        $(".layer-4").fadeIn();
-        $(".layer-3").fadeIn();
-        $(".layer-2").fadeIn();
-        $(".layer-1").fadeIn();
+        $(".layer-4").fadeIn(100);
+        $(".layer-3").fadeIn(100);
+        $(".layer-2").fadeIn(100);
+        $(".layer-1").fadeIn(100);
     });
 
-    $(".layer-4").mouseenter(function () {
+    $(".layer-4").mouseover(function () {
 
 
         $(".layer-3").hide();
@@ -64,12 +64,12 @@ $(document).ready(function () {
 
     }).mouseleave(function () {
 
-        $(".layer-3").fadeIn();
-        $(".layer-2").fadeIn();
-        $(".layer-1").fadeIn();
+        $(".layer-3").fadeIn(100);
+        $(".layer-2").fadeIn(100);
+        $(".layer-1").fadeIn(100);
     });
 
-    $(".layer-3").mouseenter(function () {
+    $(".layer-3").mouseover(function () {
 
         $(".layer-2").hide();
         $(".layer-1").hide();
@@ -77,11 +77,11 @@ $(document).ready(function () {
 
     }).mouseleave(function () {
 
-        $(".layer-2").fadeIn();
-        $(".layer-1").fadeIn();
+        $(".layer-2").fadeIn(100);
+        $(".layer-1").fadeIn(100);
     });
 
-    $(".layer-2").mouseenter(function () {
+    $(".layer-2").mouseover(function () {
 
 
         $(".layer-1").hide();
@@ -90,8 +90,85 @@ $(document).ready(function () {
     }).mouseleave(function () {
 
 
-        $(".layer-1").fadeIn();
+        $(".layer-1").fadeIn(100);
     });
+
+*/
+   $('#layer-7').click(function () {
+
+       $("#layer-6").toggle();
+       $("#layer-5").toggle();
+       $("#layer-4").toggle();
+       $("#layer-3").toggle();
+       $("#layer-2").toggle();
+       $("#layer-1").toggle();
+
+       $(".dropdown-layer-7").toggle();
+
+
+   });
+
+    $('#layer-6').click(function () {
+
+        $("#layer-5").toggle();
+        $("#layer-4").toggle();
+        $("#layer-3").toggle();
+        $("#layer-2").toggle();
+        $("#layer-1").toggle();
+
+        $(".dropdown-layer-6").toggle();
+
+
+    });
+
+    $('#layer-5').click(function () {
+
+        $("#layer-4").toggle();
+        $("#layer-3").toggle();
+        $("#layer-2").toggle();
+        $("#layer-1").toggle();
+
+        $(".dropdown-layer-5").toggle();
+
+
+    });
+
+    $('#layer-4').click(function () {
+
+        $("#layer-3").toggle();
+        $("#layer-2").toggle();
+        $("#layer-1").toggle();
+
+        $(".dropdown-layer-4").toggle();
+
+
+    });
+
+    $('#layer-3').click(function () {
+
+        $("#layer-2").toggle();
+        $("#layer-1").toggle();
+
+        $(".dropdown-layer-3").toggle();
+
+    });
+
+    $('#layer-2').click(function () {
+
+        $("#layer-1").toggle();
+
+        $(".dropdown-layer-2").toggle();
+
+
+    });
+
+    $('#layer-1').click(function () {
+
+        $(".dropdown-layer-1").toggle();
+
+
+    });
+
 
 
 });
